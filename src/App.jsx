d06aa8613235c01809326ec8f5384775df9b898a
Khaled Kamr <200013171@must.edu.eg>
2025-08-07 23:25:16 +0300

    projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
+import Projects from "./Components/Projects";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
     </div>
   );
 }
